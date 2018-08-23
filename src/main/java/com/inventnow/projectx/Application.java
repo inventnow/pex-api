51b1b31354d8f2b4260db59e448040536f3152d6
Eldrid <zztop_aditya@yahoo.com>
2018-08-23 20:28:31 +0200

    fix resource.

--- src/main/java/com/inventnow/projectx/Application.java
+++ src/main/java/com/inventnow/projectx/Application.java
@@ -2,12 +2,8 @@ package com.inventnow.projectx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
-@EnableResourceServer
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/inventnow/projectx/security/SecurityOAuth2ServerConfig.java
+++ src/main/java/com/inventnow/projectx/security/SecurityOAuth2ServerConfig.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
@@ -17,6 +17,7 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableAuthorizationServer
 public class SecurityOAuth2ServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
